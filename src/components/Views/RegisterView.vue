14a1f25a12c0620ea9dd5ed6685f0eb608416583
fsotelo2504 <fsotelo@aynitech.com>
2023-08-22 01:46:48 -0500

    removed terms and conditions modal from landing, now it's on registerView

--- src/components/Views/RegisterView.vue
+++ src/components/Views/RegisterView.vue
@@ -97,9 +97,9 @@
         placeholder="Ingresar contraseña"
         v-model="password"
       />
-      <p v-if="error">{{ error }}</p>
+      <p v-if="error" class="error-message">{{ error }}</p>
 
-      <div class="check-terms">
+      <!-- <div class="check-terms">
         <input
           type="checkbox"
           name="terms and conditions"
@@ -108,14 +108,45 @@
           class="check"
         />
         <p class="terms">Estoy de acuerdo en participar en el test</p>
-      </div>
+      </div> -->
 
-      <button class="btn_model">Registrarme</button>
+      <!-- <button class="btn_model">Registrarme</button> -->
+
+      <div class="btn_model" style="cursor: pointer;" @click="dialogTerms = true">
+        Registrarme
+      </div>
 
       <p class="have-account">
         Ya tienes cuenta?
         <router-link :to="{ name: 'Login' }">Log in</router-link>
       </p>
+
+      <!-- dialog terminos y condiciones -->
+      <v-dialog v-model="dialogTerms" max-width="700">
+        <v-card>
+          <v-card-title class="text-h5">
+            Términos y condiciones
+          </v-card-title>
+
+          <v-card-text>
+            <p>Has sido seleccionado para formar parte de un estudio sobre la estimación del nivel de pensamiento computacional. Para lograr los objetivos de esta investigación, te invitamos cordialmente a completar el siguiente cuestionario. Queremos enfatizar que tu participación es totalmente voluntaria. Todas las respuestas proporcionadas serán tratadas de manera confidencial y los resultados se utilizarán exclusivamente con fines científicos. Tu contribución ayudará a ampliar nuestro entendimiento en esta área de estudio.</p>
+            <p>Te proporcionaremos información adicional para que puedas tomar una decisión informada. Es esencial que comprendas que no hay ningún riesgo asociado con tu participación en este estudio. Si decides participar, por favor marca la casilla correspondiente. Ten la certeza de que tus respuestas y cualquier información personal se manejan con la máxima confidencialidad y sólo serán utilizadas para fines científicos.</p>
+            <p>La razón por la cual solicitamos tu consentimiento informado es asegurarnos de que comprendes plenamente los objetivos y posibles beneficios de este estudio. Estamos dispuestos a proporcionar cualquier información adicional que requieras para tomar una decisión informada.</p>
+
+            <v-checkbox
+              v-model="formData.accepted"
+              label="Estoy de acuerdo en participar en el test"
+            ></v-checkbox>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="validateTermsAndConditions">
+              Continuar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </form>
   </div>
 </template>
@@ -131,23 +162,37 @@ export default {
       email: '',
       password: '',
       passwordConfirmation: '',
+      dialogTerms: false,
 
       formData: {
         gender: 'Genero',
         age: 'age',
         Computing: 'Computing',
         Programing: 'Programing',
+        accepted: false,
       },
     };
   },
 
   methods: {
+    validateTermsAndConditions() {
+      if(this.formData.accepted) {
+        // aceptó los términos y condiciones
+        // this.$router.push({name: 'Login'}); Ya no más
+        this.submitRegister();
+        this.dialogTerms = false;
+      } else {
+        // No aceptó
+        this.dialogTerms = false;
+      }
+    },
     async submitRegister() {
       try {
         await signUp(this.email, this.password)
           .then(() => this.getSignUpData())
           .then(() => this.$router.replace({ name: 'Login' }));
       } catch (error) {
+        this.formData.accepted = false;
         this.error = error.code;
 
         switch (error.code) {
@@ -179,4 +224,14 @@ export default {
 
 <style>
 @import '../../assets/style_register.css';
+
+.error-message {
+  color: red;
+  width: 100%;
+  text-align: center;
+  background: rgba(255,0,0,0.2);
+  margin-top: 15px;
+  padding-block: 10px;
+  border-radius: 30px;
+}
 </style>
